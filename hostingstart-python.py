8d81ccdee5f63551d7ddb85a9c92110246f12623
Pedro Dias Vicente <pedrolvicente@gmail.com>
2015-05-20 15:21:46 +0100

    new one commit

--- hostingstart-python.py
+++ hostingstart-python.py
@@ -2,7 +2,7 @@ import sys
 import platform
 
 def application(environ, start_response):
-    start_response(b'200 OK', [(b'Content-Type', b'text/html')])
+    start_response(b'200 OK', [(b'Content-Type', b'text/python')])
     with open ("batalhanaval.py", "r") as hostingstart_file:
         hosting = hostingstart_file.read()
         yield hosting.encode('utf8').replace(b'PYTHON_VERSION', platform.python_version().encode('utf8'))
